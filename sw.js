9db9ebf2c9a916624a4b744bcc8bb898e55e3b8d
dogus <karssitetelekom@gmail.com>
2026-02-13 20:18:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
   sw.js - Production Service Worker with Offline Support
 */
 
-const CACHE_NAME = 'izin-v1.5';
+const CACHE_NAME = 'izin-v1.6';
 const ASSETS = [
     '/index.html',
     '/style.css',
